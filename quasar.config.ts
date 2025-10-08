88617eb9076944a8420a1d37aebafa5dc448397d
Chabiao44 <ckhemaramylol@gmail.com>
2025-10-08 10:07:39 +0700

    CI/CD

--- quasar.config.ts
+++ quasar.config.ts
@@ -39,7 +39,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/channarong-khemaram-318/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
